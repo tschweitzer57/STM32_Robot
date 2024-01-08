fe5993d29d790298b76ef9c685b2a7728db12825
tschweitzer57 <thibault.schweitzer@estaca.eu>
2024-01-08 14:20:27 +0100

    Amélioration du client

--- Client/client3.py
+++ Client/client3.py
@@ -2,15 +2,15 @@ import socket
 import threading
 from time import sleep
 
-# create a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_ip = "192.168.1.16"  # replace with the server's IP address
-server_port = 8000  # replace with the server's port number
-
-# establish connection with server
-client.connect((server_ip, server_port))
-
-
+def start_client():
+    print('\n')
+    print('            _     _      _          ___ _ _            _             _   ___  ')
+    print(' /\   /\___| |__ (_) ___| | ___    / __\ (_) ___ _ __ | |_  /\   /\ / | / _ \ ')
+    print(' \ \ / / _ \ \'_ \| |/ __| |/ _ \  / /  | | |/ _ \ \'_ \| __| \ \ / / | || | | |')
+    print('  \ V /  __/ | | | | (__| |  __/ / /___| | |  __/ | | | |_   \ V /  | || |_| |')
+    print('   \_/ \___|_| |_|_|\___|_|\___| \____/|_|_|\___|_| |_|\__|   \_/   |_(_)___/ ')
+    print('\n\n')
+    
 def send_client():
     
     while True:
@@ -44,9 +44,19 @@ def rec_client():
     client.close()
     print("Connection to server closed")
 
+#if '__name__' == '__main__':
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_ip = "192.168.1.16"  # replace with the server's IP address
+server_port = 8000  # replace with the server's port number
+
+# establish connection with server
+client.connect((server_ip, server_port))
+
 # recvThread create
 recThread = threading.Thread(target=rec_client)
 sendThread = threading.Thread(target=send_client)
 
+start_client()
 recThread.start()
 sendThread.start()
